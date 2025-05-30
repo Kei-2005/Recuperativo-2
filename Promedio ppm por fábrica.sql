817ebb9d1c4564edfb39b7f1f7087a6b6b1f89c9
Kei-2005 <keisy.valoyes678@pascualbravo.edu.co>
2025-05-30 11:53:34 -0500

    Add files via upload

---
+++ Promedio ppm por fábrica.sql
@@ -0,0 +1,10 @@
+SELECT 
+  f.nombre_fabrica AS fabrica,
+  ROUND(AVG(l.ppm), 2) AS promedio_ppm
+FROM lectura l
+JOIN sensor s ON l.id_sensor = s.id_sensor
+JOIN microcontrolador m ON s.id_micro = m.id_micro
+JOIN linea_produccion lp ON m.id_linea = lp.id_linea
+JOIN fabrica f ON lp.id_fabrica = f.id_fabrica
+GROUP BY f.nombre_fabrica
+ORDER BY promedio_ppm DESC;
